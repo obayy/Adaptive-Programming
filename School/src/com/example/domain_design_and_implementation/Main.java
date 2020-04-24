1efa165cd6d4df47680bfcf324285250e469635c
obayy <obay.elkoukouchi@student.hu.nl>
2020-04-24 20:11:37 +0200

    - Implemented interface Persoon

---
+++ School/src/com/example/domain_design_and_implementation/Main.java
@@ -0,0 +1,9 @@
+package com.example.domain_design_and_implementation;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+    }
+
+}
---
+++ School/src/com/example/domain_design_and_implementation/Persoon.java
@@ -0,0 +1,8 @@
+package com.example.domain_design_and_implementation;
+
+public interface Persoon {
+    public String getNaam();
+    public void setNaam(String naam);
+    public int getLeeftijd();
+    public void setLeeftijd(int leeftijd);
+}
